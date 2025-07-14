482bce13fa2680af42847c6194454dec9eec7086
Shubhodeep De <163862265+shubh-man007@users.noreply.github.com>
2025-07-14 23:57:21 +0530

    Create longestConsecutive.py

---
+++ arrays/longestConsecutive.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+
+        num_set = set(nums)
+        longest = 0
+
+        for num in num_set:
+            if num - 1 not in num_set:
+                current_num = num
+                current_streak = 1
+
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
+
+                longest = max(longest, current_streak)
+
+        return longest
