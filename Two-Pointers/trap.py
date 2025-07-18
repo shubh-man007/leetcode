26b9fe6b78cd4d6eeb02ea2bdc67cc981e37c6c9
Shubhodeep De <163862265+shubh-man007@users.noreply.github.com>
2025-07-18 23:53:10 +0530

    Create trap.py

---
+++ Two-Pointers/trap.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+
+        if not height: return 0
+
+        l, r = 0, len(height) - 1
+        lMax, rMax = height[l], height[r]
+        res = 0
+
+        while l < r:
+            if lMax < rMax:
+                l += 1
+                lMax = max(lMax, height[l])
+                res += lMax - height[l]
+            else:
+                r -= 1
+                rMax = max(rMax, height[r])
+                res += rMax - height[r]
+
+        return res
