fce61378ad1b94b34b0ee707a914d63b0b8de488
Shubhodeep De <163862265+shubh-man007@users.noreply.github.com>
2025-07-21 13:17:54 +0530

    Update quickSort.py

--- Sorting/quickSort.py
+++ Sorting/quickSort.py
@@ -6,8 +6,12 @@ class Solution(object):
         """
         
     for i in range(0, len(nums) - 1):
+        count = 0
         for j in range(0, len(nums)-1 - i):
             if nums[j] > nums[j+1]:
                 nums[j], nums[j+1] = nums[j+1], nums[j]
+                count += 1
+        if count == 0:
+            break
 
     return nums
