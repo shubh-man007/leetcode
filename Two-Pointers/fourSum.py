7a56bb1853f7964acd9d2aefe66a9eefca1b1e09
Shubhodeep De <163862265+shubh-man007@users.noreply.github.com>
2025-07-28 22:30:43 +0530

    Create fourSum.py

---
+++ Two-Pointers/fourSum.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        res = []
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for j in range(i + 1, len(nums)):
+                if j > i + 1 and nums[j] == nums[j-1]:  
+                    continue
+                l, r = j + 1, len(nums) - 1
+                while l < r:
+                    total = nums[i] + nums[j] + nums[l] + nums[r]
+                    
+                    if total < target:
+                        l += 1
+                    elif total > target:
+                        r -= 1
+                    else:
+                        res.append([nums[i], nums[j], nums[l], nums[r]])
+                        l += 1
+                        r -= 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
+                        while l < r and nums[r] == nums[r+1]: 
+                            r -= 1
+
+        return res 
