039db10920e41c4032c562ddccc2f21eb9ba9342
Shubhodeep De <163862265+shubh-man007@users.noreply.github.com>
2025-07-29 20:21:40 +0530

    Create merge.py

---
+++ arrays/merge.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        if not intervals:
+            return []
+        
+        intervals.sort(key=lambda x: x[0])
+        res = [intervals[0]]
+
+        for i in range(1, len(intervals)):
+            if res[-1][1] >= intervals[i][0]:
+                res[-1][1] = max(res[-1][1], intervals[i][1])
+            else:
+                res.append(intervals[i])
+        
+        return res
